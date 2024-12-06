96b9d47ceceee15f3f1e2fb7d67044306fdafa1b
Mathieu Gonçalves <mathieu.goncalves.45120@gmail.com>
2024-12-06 14:58:58 +0100

    No idea what happened on this bracket

--- code/game/g_items.cpp
+++ code/game/g_items.cpp
@@ -860,7 +860,7 @@ void FinishSpawningItem( gentity_t *ent ) {
 				itemNew = bg_itemlist + rng;
 			}
 			if (itemNew->giType == IT_HOLOCRON || itemNew->giTag == WP_SABER) // In case we roll a saber or an holocron, we shall roll a 33/66 to keep the item or not
-      {
+			{
 				rng = rand() % 3;
 				if (!rng) // We rolled a 0, reroll once
 				{
