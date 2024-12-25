7657be64b72f259bd171e6c1a9fffdd35c1ad128
Mathieu Gonçalves <81418700+ArrayIndexOutOfBound@users.noreply.github.com>
2024-12-25 12:03:26 +0100

    Making randomized secutiry keys usable, can be transfered between levels. (#13)

--- code/game/g_inventory.cpp
+++ code/game/g_inventory.cpp
@@ -5,6 +5,9 @@
 
 #include "g_local.h"
 
+// Randomizer
+extern	vmCvar_t		cg_enableRandomizer;
+
 /*
 ================
 Goodie Keys
@@ -62,18 +65,54 @@ Security Keys
 */
 qboolean INV_SecurityKeyGive( gentity_t *target, const char *keyname )
 {
-	if ( target == NULL || keyname == NULL || target->client == NULL )
+	if (!cg_enableRandomizer.integer)
 	{
+		if (target == NULL || keyname == NULL || target->client == NULL)
+		{
+			return qfalse;
+		}
+
+		for (int i = 0; i <= 4; i++)
+		{
+			if (target->client->ps.security_key_message[i][0] == NULL)
+			{//fill in the first empty slot we find with this key
+				target->client->ps.inventory[INV_SECURITY_KEY]++;	// He got the key
+				Q_strncpyz(target->client->ps.security_key_message[i], keyname, MAX_SECURITY_KEY_MESSSAGE, qtrue);
+				return qtrue;
+			}
+		}
+		//couldn't find an empty slot
 		return qfalse;
 	}
-
-	for ( int i = 0; i <= 4; i++ )
+	else // Randomizer, make a random taken key universally usable
 	{
-		if ( target->client->ps.security_key_message[i][0] == NULL )
-		{//fill in the first empty slot we find with this key
-			target->client->ps.inventory[INV_SECURITY_KEY]++;	// He got the key
-			Q_strncpyz( target->client->ps.security_key_message[i], keyname, MAX_SECURITY_KEY_MESSSAGE, qtrue );
-			return qtrue;
+		if (keyname != NULL) // Normal key, normal behavior
+		{
+			for (int i = 0; i <= 4; i++)
+			{
+				if (target->client->ps.security_key_message[i][0] == NULL)
+				{//fill in the first empty slot we find with this key
+					target->client->ps.inventory[INV_SECURITY_KEY]++;	// He got the key
+					Q_strncpyz(target->client->ps.security_key_message[i], keyname, MAX_SECURITY_KEY_MESSSAGE, qtrue);
+					return qtrue;
+				}
+			}
+			//couldn't find an empty slot
+			return qfalse;
+		}
+		else // Randomly spawned key
+		{
+			for (int i = 0; i <= 4; i++)
+			{
+				if (target->client->ps.security_key_message[i][0] == NULL)
+				{//fill in the first empty slot we find with this key
+					target->client->ps.inventory[INV_SECURITY_KEY]++;	// He got the key
+					Q_strncpyz(target->client->ps.security_key_message[i], "rando_key", MAX_SECURITY_KEY_MESSSAGE, qtrue);
+					return qtrue;
+				}
+			}
+			//couldn't find an empty slot
+			return qfalse;
 		}
 	}
 	//couldn't find an empty slot
@@ -82,56 +121,111 @@ qboolean INV_SecurityKeyGive( gentity_t *target, const char *keyname )
 
 void INV_SecurityKeyTake( gentity_t *target, char *keyname )
 {
-	if ( !target || !keyname || !target->client )
+	if (!cg_enableRandomizer.integer)
 	{
-		return;
-	}
-
-	for ( int i = 0; i <= 4; i++ )
-	{
-		if ( target->client->ps.security_key_message[i] )
+		if (!target || !keyname || !target->client)
 		{
-			if ( !Q_stricmp( keyname, target->client->ps.security_key_message[i] ) )
+			return;
+		}
+		for (int i = 0; i <= 4; i++)
+		{
+			if (target->client->ps.security_key_message[i])
 			{
-				target->client->ps.inventory[INV_SECURITY_KEY]--;	// Take the key
-				target->client->ps.security_key_message[i][0] = NULL;
-				return;
+				if (!Q_stricmp(keyname, target->client->ps.security_key_message[i]))
+				{
+					target->client->ps.inventory[INV_SECURITY_KEY]--;	// Take the key
+					target->client->ps.security_key_message[i][0] = NULL;
+					return;
+				}
+			}
+			/*
+			//don't do this because we could have removed one that's between 2 valid ones
+			else
+			{
+				break;
+			}
+			*/
+		}
+	}
+	else // Randomizer, make a random taken key universally usable
+	{
+		for (int i = 0; i <= 4; i++) // Check for normal key first
+		{
+			if (target->client->ps.security_key_message[i])
+			{
+				if (!Q_stricmp(keyname, target->client->ps.security_key_message[i]))
+				{
+					target->client->ps.inventory[INV_SECURITY_KEY]--;	// Take the key
+					target->client->ps.security_key_message[i][0] = NULL;
+					return;
+				}
 			}
 		}
-		/*
-		//don't do this because we could have removed one that's between 2 valid ones
-		else
+		for (int i = 0; i <= 4; i++) // Check for rando_key
 		{
-			break;
+			if (target->client->ps.security_key_message[i])
+			{
+				if (!Q_stricmp("rando_key", target->client->ps.security_key_message[i]))
+				{
+					target->client->ps.inventory[INV_SECURITY_KEY]--;	// Take the key
+					target->client->ps.security_key_message[i][0] = NULL;
+					return;
+				}
+			}
 		}
-		*/
 	}
+	
 }
 
 qboolean INV_SecurityKeyCheck( gentity_t *target, char *keyname )
 {
-	if ( !target || !keyname || !target->client )
+	if (!cg_enableRandomizer.integer)
 	{
-		return (qfalse);
-	}
-
-	for ( int i = 0; i <= 4; i++ )
-	{
-		if ( target->client->ps.inventory[INV_SECURITY_KEY] && target->client->ps.security_key_message[i] )
+		if (!target || !keyname || !target->client)
 		{
-			if ( !Q_stricmp( keyname, target->client->ps.security_key_message[i] ) )
+			return (qfalse);
+		}
+		for (int i = 0; i <= 4; i++)
+		{
+			if (target->client->ps.inventory[INV_SECURITY_KEY] && target->client->ps.security_key_message[i])
 			{
-				return (qtrue);
+				if (!Q_stricmp(keyname, target->client->ps.security_key_message[i]))
+				{
+					return (qtrue);
+				}
+			}
+			/*
+			//don't do this because we could have removed one that's between 2 valid ones
+			else
+			{
+				break;
+			}
+			*/
+		}
+	}
+	else // Randomizer, make a random taken key universally usable
+	{
+		for (int i = 0; i <= 4; i++) // Check for normal key first
+		{
+			if (target->client->ps.inventory[INV_SECURITY_KEY] && target->client->ps.security_key_message[i])
+			{
+				if (!Q_stricmp(keyname, target->client->ps.security_key_message[i]))
+				{
+					return (qtrue);
+				}
 			}
 		}
-		/*
-		//don't do this because we could have removed one that's between 2 valid ones
-		else
+		for (int i = 0; i <= 4; i++) // Check for rando_key
 		{
-			break;
+			if (target->client->ps.inventory[INV_SECURITY_KEY] && target->client->ps.security_key_message[i])
+			{
+				if (!Q_stricmp("rando_key", target->client->ps.security_key_message[i]))
+				{
+					return (qtrue);
+				}
+			}
 		}
-		*/
-	}
 
+	}
 	return (qfalse);
 }
